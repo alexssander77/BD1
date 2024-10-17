4dfea2856ca683fb87c6801dad1a98f4f138423e
Alexssander José de Oliveira de Castro <147998833+alexssander77@users.noreply.github.com>
2024-10-16 22:00:37 -0300

    Update Banco.sql

--- MySQL/Banco.sql
+++ MySQL/Banco.sql
@@ -35,7 +35,8 @@ cpf varchar(14),
 num_conta varchar(7),
 primary key(cpf,num_conta),
 foreign key (cpf) references cliente(cpf),
-foreign key (num_conta) references conta(num_conta)
+foreign key (num_conta) references conta(num_conta),
+data_inicio date
 );
 
 create table telefone_cliente(
@@ -43,4 +44,21 @@ cpf_cli varchar(14),
 telefone varchar(20),
 primary key(cpf_cli,telefone),
 foreign key (cpf_cli) references cliente(cpf)
+
 );
+
+insert into banco (cod_banco,nome) values (1,"Banco do Brasil");
+insert into banco (cod_banco,nome) values (4,"CEF");
+insert into agencia (num_agencia,endereco,cod_banco) values (0562,"Rua Joaquim Teixeira Alves, 1555",4);
+insert into agencia (num_agencia,endereco,cod_banco) values (3153,"Av Marcelino Pires, 1960",1);
+insert into cliente (cpf,nome,sexo,endereco) values ("111.222.333-44","Jennifer B Souza",'F',"Rua Cuiabá, 1050");
+insert into cliente (cpf,nome,sexo,endereco) values ("666.777.888-99","Caetano K Lima",'M',"Rua Ivinhema, 879");
+insert into cliente (cpf,nome,sexo,endereco) values ("555.444.777-33","Silvia Macedo",'F',"Rua Estados Unidos, 735");
+insert into conta(num_conta,saldo,tipo_conta,num_agencia) values ("86340-2",763.05,2,3153);
+insert into conta(num_conta,saldo,tipo_conta,num_agencia) values ("23584-7",3879.12,1,0562);
+insert into historico(cpf,num_conta,data_inicio) values("111.222.333-44","23584-7","1997-12-17");
+insert into historico(cpf,num_conta,data_inicio) values("666.777.888-99","23584-7","1997-12-17");
+insert into historico(cpf,num_conta,data_inicio) values("555.444.777-33","86340-2","2010-11-29");
+insert into telefone_cliente(cpf_cli,telefone) values("111.222.333-44","(67)3422-7788");
+insert into telefone_cliente(cpf_cli,telefone) values("666.777.888-99","(67)3423-9900");
+insert into telefone_cliente(cpf_cli,telefone) values("666.777.888-99","(67)8121-8833");
